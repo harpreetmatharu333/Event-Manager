27615bd9fbbe5e984ef84dd784fd384dafe1f016
mansi jain <mansi.jain.cs.2018@miet.ac.in>
2022-12-13 15:58:50 +0530

    feed page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeedComponent } from './feed/feed.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FeedComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
